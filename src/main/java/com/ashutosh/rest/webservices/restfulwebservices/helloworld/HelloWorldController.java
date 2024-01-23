ac328a199a0e97b300ec56a706e73d1ca8e31c9f
Ashutosh Giri <ashutoshgiri249@gmail.com>
2024-01-23 16:23:48 +0530

    add openapi

--- src/main/java/com/ashutosh/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/ashutosh/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,9 +1,19 @@
 package com.ashutosh.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path="/hello-world")
     public String helloworld(){
@@ -18,5 +28,12 @@ public class HelloWorldController {
     @GetMapping(path="/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean("Hello world " + name);
+    }
+
+    @GetMapping(path="/hello-world-internationalized")
+    public String helloworldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+        //return "Hello world";
     }
 }
