5c42d8ba5bc70184b17d12ed596494ed75b8b395
Ashutosh Giri <ashutoshgiri249@gmail.com>
2024-01-24 22:01:15 +0530

    done actuator, HAL explorer, h2-db, post entity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,6 @@ values(10002, current_date(), 'Vinod');
 
 insert into user_details(id, birth_date, name)
 values(10003, current_date(), 'Akash');
+
+insert into post(id, description, user_id)
+values (20001, 'My First Post', 10001);
